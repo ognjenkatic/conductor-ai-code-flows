237345e6fbd686dcd676c386a47d24dbc0dce915
Ognjen Katić <44910579+ognjenkatic@users.noreply.github.com>
2024-08-11 19:40:03 +0200

    fix prompt mutations

--- src/CodeFlows/CodeFlows.GenAi/OpenAi/Workers/RefactorProject.cs
+++ src/CodeFlows/CodeFlows.GenAi/OpenAi/Workers/RefactorProject.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
+﻿using System.ComponentModel.DataAnnotations;
 using System.Text;
-using System.Threading.Tasks;
 using CodeFlows.GenAi.Common.Util;
 using CodeFlows.GenAi.OpenAi.DTOs;
 using ConductorSharp.Engine;
 using ConductorSharp.Engine.Builders.Metadata;
 using MediatR;
+using Microsoft.Extensions.Logging;
 using OpenAI_API;
 using OpenAI_API.Chat;
-using OpenAI_API.Models;
 
 namespace CodeFlows.GenAi.OpenAi.Workers
 {
@@ -33,18 +29,12 @@ namespace CodeFlows.GenAi.OpenAi.Workers
         }
 
         [OriginalName("ai_refactor_project")]
-        public class Handler : TaskRequestHandler<RefactorProject, Response>
+        public class Handler(OpenAIAPI openAiClient, ILogger<Handler> logger)
+            : TaskRequestHandler<RefactorProject, Response>
         {
             private const string fileNamePlaceholder = "{filename}";
             private const string issuesPlaceholder = "{issues}";
             private const string sourcecodePlaceholder = "{sourcecode}";
-
-            private readonly OpenAIAPI openAiClient;
-
-            public Handler(OpenAIAPI openAiClient)
-            {
-                this.openAiClient = openAiClient;
-            }
 
             public override async Task<Response> Handle(
                 RefactorProject request,
@@ -85,46 +75,57 @@ namespace CodeFlows.GenAi.OpenAi.Workers
 
                     foreach (var fileIssues in filesToProcess)
                     {
+                        var sb = new StringBuilder(refactorPrompt);
+
                         var file = Path.Join(projectDir, fileIssues.Key.Split(":")[1]);
                         var issues = fileIssues.Value;
 
-                        refactorPrompt = refactorPrompt.Replace(fileNamePlaceholder, file);
-                        refactorPrompt = refactorPrompt.Replace(
-                            issuesPlaceholder,
-                            BuildIssueParagraph(issues)
-                        );
-                        refactorPrompt = refactorPrompt.Replace(
-                            sourcecodePlaceholder,
-                            File.ReadAllText(file)
-                        );
+                        sb = sb.Replace(fileNamePlaceholder, file);
+                        sb = sb.Replace(issuesPlaceholder, BuildIssueParagraph(issues));
+                        sb = sb.Replace(sourcecodePlaceholder, File.ReadAllText(file));
 
-                        tasks.Add(GenerateNewCodeAndWriteToFile(file, refactorPrompt));
+                        tasks.Add(TryGenerateNewCodeAndWriteToFile(file, sb.ToString()));
                     }
 
                     await Task.WhenAll(tasks);
 
                     processedPrompts += filesToProcess.Count;
+
+                    logger.LogDebug(
+                        "Refactored {refactorCount} of {totalCount} files",
+                        processedPrompts,
+                        request.IssuesByFile.Count
+                    );
                 }
 
                 return new Response() { RefactoredFiles = new() };
             }
 
-            private async Task GenerateNewCodeAndWriteToFile(string filePath, string prompt)
+            private async Task TryGenerateNewCodeAndWriteToFile(string filePath, string prompt)
             {
-                var convo = openAiClient.Chat.CreateConversation(
-                    new ChatRequest
-                    {
-                        Temperature = 0.5,
-                        MaxTokens = 4096,
-                        Model = "gpt-4o"
-                    }
-                );
+                logger.LogDebug("Beginning Refactor: {filePath}", filePath);
+                try
+                {
+                    var convo = openAiClient.Chat.CreateConversation(
+                        new ChatRequest
+                        {
+                            Temperature = 0.5,
+                            MaxTokens = 4096,
+                            Model = "gpt-4o"
+                        }
+                    );
 
-                convo.AppendUserInput(prompt);
+                    convo.AppendUserInput(prompt);
 
-                var newCode = await convo.GetResponseFromChatbotAsync();
+                    var newCode = await convo.GetResponseFromChatbotAsync();
 
-                File.WriteAllText(filePath, newCode);
+                    File.WriteAllText(filePath, newCode);
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError("Failed Refactoring: {filePath} due to {error}", filePath, ex);
+                }
+                logger.LogDebug("Finished Refactor: {filePath}", filePath);
             }
 
             private static string BuildIssueParagraph(List<CodeFlowIssue> issues)
